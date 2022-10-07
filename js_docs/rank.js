d4954accde1a24e7d379397244fdf8d7d0ffb930
EscolaEducaRS <65333639+EscolaEducaRS@users.noreply.github.com>
2022-10-07 15:30:22 -0300

    Add files via upload

--- js_docs/rank.js
+++ js_docs/rank.js
@@ -1,58 +1,276 @@
-let grupo1 = "JYLEGH"
-let grupo2 = "Chico Rafael"
-let grupo3 = "Irmandade"
-let grupo4 = "Os Intocáveis"
-let grupo5 = "Os Elevados"
-let grupo6 = "Mathematics"
-let grupo7 = "Ciclone"
-let grupo8 = "Cocotas"
-let grupo9 = "Sem Cadastro!"
-let grupo10 = "Sem Cadastro!"
+let grupo1 = {
+    nome: "JYLEGH",
+    notas: [0, 0.75],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo1.notas[1]>0) {notasM = grupo1.notas[1] }
+        if (grupo1.notas[2]>0) {notasM += grupo1.notas[2]}
+        if (grupo1.notas[3]>0) {notasM += grupo1.notas[3]}
+        if (grupo1.notas[4]>0) {notasM += grupo1.notas[4]}
+        if (grupo1.notas[5]>0) {notasM += grupo1.notas[5]}
+        if (grupo1.notas[6]>0) {notasM += grupo1.notas[6]}
+        if (grupo1.notas[7]>0) {notasM += grupo1.notas[7]}
+        if (grupo1.notas[8]>0) {notasM += grupo1.notas[8]}
+        if (grupo1.notas[9]>0) {notasM += grupo1.notas[9]}
+        if (grupo1.notas[10]>0) {notasM += grupo1.notas[10]}
+//
+        if (grupo1.notas.length>1){
+            notasM = notasM / (grupo1.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo2 = {
+    nome: "Chico Rafael",
+    notas: [0, 1],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo2.notas[1]>0) {notasM = grupo2.notas[1] }
+        if (grupo2.notas[2]>0) {notasM += grupo2.notas[2]}
+        if (grupo2.notas[3]>0) {notasM += grupo2.notas[3]}
+        if (grupo2.notas[4]>0) {notasM += grupo2.notas[4]}
+        if (grupo2.notas[5]>0) {notasM += grupo2.notas[5]}
+        if (grupo2.notas[6]>0) {notasM += grupo2.notas[6]}
+        if (grupo2.notas[7]>0) {notasM += grupo2.notas[7]}
+        if (grupo2.notas[8]>0) {notasM += grupo2.notas[8]}
+        if (grupo2.notas[9]>0) {notasM += grupo2.notas[9]}
+        if (grupo2.notas[10]>0) {notasM += grupo2.notas[10]}
+//
+        if (grupo2.notas.length>1){
+            notasM = notasM / (grupo2.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo3 = {
+    nome: "Irmandade",
+    notas: [0, 1],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo3.notas[1]>0) {notasM = grupo3.notas[1] }
+        if (grupo3.notas[2]>0) {notasM += grupo3.notas[2]}
+        if (grupo3.notas[3]>0) {notasM += grupo3.notas[3]}
+        if (grupo3.notas[4]>0) {notasM += grupo3.notas[4]}
+        if (grupo3.notas[5]>0) {notasM += grupo3.notas[5]}
+        if (grupo3.notas[6]>0) {notasM += grupo3.notas[6]}
+        if (grupo3.notas[7]>0) {notasM += grupo3.notas[7]}
+        if (grupo3.notas[8]>0) {notasM += grupo3.notas[8]}
+        if (grupo3.notas[9]>0) {notasM += grupo3.notas[9]}
+        if (grupo3.notas[10]>0) {notasM += grupo3.notas[10]}
+//
+        if (grupo3.notas.length>1){
+            notasM = notasM / (grupo3.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo4 = {
+    nome: "Os Intocáveis",
+    notas: [0, 0.5],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo4.notas[1]>0) {notasM = grupo4.notas[1] }
+        if (grupo4.notas[2]>0) {notasM += grupo4.notas[2]}
+        if (grupo4.notas[3]>0) {notasM += grupo4.notas[3]}
+        if (grupo4.notas[4]>0) {notasM += grupo4.notas[4]}
+        if (grupo4.notas[5]>0) {notasM += grupo4.notas[5]}
+        if (grupo4.notas[6]>0) {notasM += grupo4.notas[6]}
+        if (grupo4.notas[7]>0) {notasM += grupo4.notas[7]}
+        if (grupo4.notas[8]>0) {notasM += grupo4.notas[8]}
+        if (grupo4.notas[9]>0) {notasM += grupo4.notas[9]}
+        if (grupo4.notas[10]>0) {notasM += grupo4.notas[10]}
+//
+        if (grupo4.notas.length>1){
+            notasM = notasM / (grupo4.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo5 = {
+    nome: "Os Elevados",
+    notas: [0, 0.75],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo5.notas[1]>0) {notasM = grupo5.notas[1] }
+        if (grupo5.notas[2]>0) {notasM += grupo5.notas[2]}
+        if (grupo5.notas[3]>0) {notasM += grupo5.notas[3]}
+        if (grupo5.notas[4]>0) {notasM += grupo5.notas[4]}
+        if (grupo5.notas[5]>0) {notasM += grupo5.notas[5]}
+        if (grupo5.notas[6]>0) {notasM += grupo5.notas[6]}
+        if (grupo5.notas[7]>0) {notasM += grupo5.notas[7]}
+        if (grupo5.notas[8]>0) {notasM += grupo5.notas[8]}
+        if (grupo5.notas[9]>0) {notasM += grupo5.notas[9]}
+        if (grupo5.notas[10]>0) {notasM += grupo5.notas[10]}
+//
+        if (grupo5.notas.length>1){
+            notasM = notasM / (grupo5.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo6 = {
+    nome: "Mathematics",
+    notas: [0],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo6.notas[1]>0) {notasM = grupo6.notas[1] }
+        if (grupo6.notas[2]>0) {notasM += grupo6.notas[2]}
+        if (grupo6.notas[3]>0) {notasM += grupo6.notas[3]}
+        if (grupo6.notas[4]>0) {notasM += grupo6.notas[4]}
+        if (grupo6.notas[5]>0) {notasM += grupo6.notas[5]}
+        if (grupo6.notas[6]>0) {notasM += grupo6.notas[6]}
+        if (grupo6.notas[7]>0) {notasM += grupo6.notas[7]}
+        if (grupo6.notas[8]>0) {notasM += grupo6.notas[8]}
+        if (grupo6.notas[9]>0) {notasM += grupo6.notas[9]}
+        if (grupo6.notas[10]>0) {notasM += grupo6.notas[10]}
+//
+        if (grupo6.notas.length>1){
+            notasM = notasM / (grupo6.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100;
+    }
+}
+let grupo7 = {
+    nome: "Ciclone",
+    notas: [0],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo7.notas[1]>0) {notasM = grupo7.notas[1] }
+        if (grupo7.notas[2]>0) {notasM += grupo7.notas[2]}
+        if (grupo7.notas[3]>0) {notasM += grupo7.notas[3]}
+        if (grupo7.notas[4]>0) {notasM += grupo7.notas[4]}
+        if (grupo7.notas[5]>0) {notasM += grupo7.notas[5]}
+        if (grupo7.notas[6]>0) {notasM += grupo7.notas[6]}
+        if (grupo7.notas[7]>0) {notasM += grupo7.notas[7]}
+        if (grupo7.notas[8]>0) {notasM += grupo7.notas[8]}
+        if (grupo7.notas[9]>0) {notasM += grupo7.notas[9]}
+        if (grupo7.notas[10]>0) {notasM += grupo7.notas[10]}
+//
+        if (grupo7.notas.length>1){
+            notasM = notasM / (grupo7.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo8 = {
+    nome: "Cocotas",
+    notas: [0],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo8.notas[1]>0) {notasM = grupo8.notas[1] }
+        if (grupo8.notas[2]>0) {notasM += grupo8.notas[2]}
+        if (grupo8.notas[3]>0) {notasM += grupo8.notas[3]}
+        if (grupo8.notas[4]>0) {notasM += grupo8.notas[4]}
+        if (grupo8.notas[5]>0) {notasM += grupo8.notas[5]}
+        if (grupo8.notas[6]>0) {notasM += grupo8.notas[6]}
+        if (grupo8.notas[7]>0) {notasM += grupo8.notas[7]}
+        if (grupo8.notas[8]>0) {notasM += grupo8.notas[8]}
+        if (grupo8.notas[9]>0) {notasM += grupo8.notas[9]}
+        if (grupo8.notas[10]>0) {notasM += grupo8.notas[10]}
+//
+        if (grupo8.notas.length>1){
+            notasM = notasM / (grupo8.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo9 = {
+    nome: "Sem Cadastro!",
+    notas: [0],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo9.notas[1]>0) {notasM = grupo9.notas[1] }
+        if (grupo9.notas[2]>0) {notasM += grupo9.notas[2]}
+        if (grupo9.notas[3]>0) {notasM += grupo9.notas[3]}
+        if (grupo9.notas[4]>0) {notasM += grupo9.notas[4]}
+        if (grupo9.notas[5]>0) {notasM += grupo9.notas[5]}
+        if (grupo9.notas[6]>0) {notasM += grupo9.notas[6]}
+        if (grupo9.notas[7]>0) {notasM += grupo9.notas[7]}
+        if (grupo9.notas[8]>0) {notasM += grupo9.notas[8]}
+        if (grupo9.notas[9]>0) {notasM += grupo9.notas[9]}
+        if (grupo9.notas[10]>0) {notasM += grupo9.notas[10]}
+//
+        if (grupo9.notas.length>1){
+            notasM = notasM / (grupo9.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
+let grupo10 = {
+    nome: "Sem Cadastro!",
+    notas: [0],
+    mediaProv: function () {
+        let notasM = 0
+        if (grupo10.notas[1]>0) {notasM = grupo10.notas[1] }
+        if (grupo10.notas[2]>0) {notasM += grupo10.notas[2]}
+        if (grupo10.notas[3]>0) {notasM += grupo10.notas[3]}
+        if (grupo10.notas[4]>0) {notasM += grupo10.notas[4]}
+        if (grupo10.notas[5]>0) {notasM += grupo10.notas[5]}
+        if (grupo10.notas[6]>0) {notasM += grupo10.notas[6]}
+        if (grupo10.notas[7]>0) {notasM += grupo10.notas[7]}
+        if (grupo10.notas[8]>0) {notasM += grupo10.notas[8]}
+        if (grupo10.notas[9]>0) {notasM += grupo10.notas[9]}
+        if (grupo10.notas[10]>0) {notasM += grupo10.notas[10]}
+//
+        if (grupo10.notas.length>1){
+            notasM = notasM / (grupo10.notas.length - 1)
+        }
+        return Math.round(notasM * 100) / 100
+    }
+}
 
-const NGrupo1 = document.getElementById('nGrupo1').innerText = grupo1;
-const NGrupo2 = document.getElementById('nGrupo2').innerText = grupo2;
-const NGrupo3 = document.getElementById('nGrupo3').innerText = grupo3;
-const NGrupo4 = document.getElementById('nGrupo4').innerText = grupo4;
-const NGrupo5 = document.getElementById('nGrupo5').innerText = grupo5;
-const NGrupo6 = document.getElementById('nGrupo6').innerText = grupo6;
-const NGrupo7 = document.getElementById('nGrupo7').innerText = grupo7;
-const NGrupo8 = document.getElementById('nGrupo8').innerText = grupo8;
-const NGrupo9 = document.getElementById('nGrupo9').innerText = grupo9;
-const NGrupo10 = document.getElementById('nGrupo10').innerText = grupo10;
-const NGrupo11 = document.getElementById('nGrupo11').innerText = grupo1;
-const NGrupo20 = document.getElementById('nGrupo20').innerText = grupo2;
-const NGrupo30 = document.getElementById('nGrupo30').innerText = grupo3;
-const NGrupo40 = document.getElementById('nGrupo40').innerText = grupo4;
-const NGrupo50 = document.getElementById('nGrupo50').innerText = grupo5;
-const NGrupo60 = document.getElementById('nGrupo60').innerText = grupo6;
-const NGrupo70 = document.getElementById('nGrupo70').innerText = grupo7;
-const NGrupo80 = document.getElementById('nGrupo80').innerText = grupo8;
-const NGrupo90 = document.getElementById('nGrupo90').innerText = grupo9;
-const NGrupo100 = document.getElementById('nGrupo100').innerText = grupo10;
-const NGrupo12 = document.getElementById('nGrupo12').innerText = grupo1;
-const NGrupo21 = document.getElementById('nGrupo21').innerText = grupo2;
-const NGrupo31 = document.getElementById('nGrupo31').innerText = grupo3;
-const NGrupo41 = document.getElementById('nGrupo41').innerText = grupo4;
-const NGrupo51 = document.getElementById('nGrupo51').innerText = grupo5;
-const NGrupo61 = document.getElementById('nGrupo61').innerText = grupo6;
-const NGrupo71 = document.getElementById('nGrupo71').innerText = grupo7;
-const NGrupo81 = document.getElementById('nGrupo81').innerText = grupo8;
-const NGrupo91 = document.getElementById('nGrupo91').innerText = grupo9;
-const NGrupo101 = document.getElementById('nGrupo101').innerText = grupo10;
+const NGrupo1 = document.getElementById('nGrupo1').innerText = grupo1.nome;
+const NGrupo2 = document.getElementById('nGrupo2').innerText = grupo2.nome;
+const NGrupo3 = document.getElementById('nGrupo3').innerText = grupo3.nome;
+const NGrupo4 = document.getElementById('nGrupo4').innerText = grupo4.nome;
+const NGrupo5 = document.getElementById('nGrupo5').innerText = grupo5.nome;
+const NGrupo6 = document.getElementById('nGrupo6').innerText = grupo6.nome;
+const NGrupo7 = document.getElementById('nGrupo7').innerText = grupo7.nome;
+const NGrupo8 = document.getElementById('nGrupo8').innerText = grupo8.nome;
+const NGrupo9 = document.getElementById('nGrupo9').innerText = grupo9.nome;
+const NGrupo10 = document.getElementById('nGrupo10').innerText = grupo10.nome;
+const NGrupo11 = document.getElementById('nGrupo11').innerText = grupo1.nome;
+const NGrupo20 = document.getElementById('nGrupo20').innerText = grupo2.nome;
+const NGrupo30 = document.getElementById('nGrupo30').innerText = grupo3.nome;
+const NGrupo40 = document.getElementById('nGrupo40').innerText = grupo4.nome;
+const NGrupo50 = document.getElementById('nGrupo50').innerText = grupo5.nome;
+const NGrupo60 = document.getElementById('nGrupo60').innerText = grupo6.nome;
+const NGrupo70 = document.getElementById('nGrupo70').innerText = grupo7.nome;
+const NGrupo80 = document.getElementById('nGrupo80').innerText = grupo8.nome;
+const NGrupo90 = document.getElementById('nGrupo90').innerText = grupo9.nome;
+const NGrupo100 = document.getElementById('nGrupo100').innerText = grupo10.nome;
+const NGrupo12 = document.getElementById('nGrupo12').innerText = grupo1.nome;
+const NGrupo21 = document.getElementById('nGrupo21').innerText = grupo2.nome;
+const NGrupo31 = document.getElementById('nGrupo31').innerText = grupo3.nome;
+const NGrupo41 = document.getElementById('nGrupo41').innerText = grupo4.nome;
+const NGrupo51 = document.getElementById('nGrupo51').innerText = grupo5.nome;
+const NGrupo61 = document.getElementById('nGrupo61').innerText = grupo6.nome;
+const NGrupo71 = document.getElementById('nGrupo71').innerText = grupo7.nome;
+const NGrupo81 = document.getElementById('nGrupo81').innerText = grupo8.nome;
+const NGrupo91 = document.getElementById('nGrupo91').innerText = grupo9.nome;
+const NGrupo101 = document.getElementById('nGrupo101').innerText = grupo10.nome;
 
-const NG1_1 = document.getElementById('NotaG1_1').innerText = '0.75';
-const NG2_1 = document.getElementById('NotaG2_1').innerText = '1';
-const NG3_1 = document.getElementById('NotaG3_1').innerText = '1';
-const NG4_1 = document.getElementById('NotaG4_1').innerText = '0.5';
-document.getElementById("NotaG4_1").style.color = "yellow";
-const NG5_1 = document.getElementById('NotaG5_1').innerText = '0.75';
-const NG6_1 = document.getElementById('NotaG6_1').innerText = '';
-const NG7_1 = document.getElementById('NotaG7_1').innerText = '';
-const NG8_1 = document.getElementById('NotaG8_1').innerText = '';
-const NG9_1 = document.getElementById('NotaG9_1').innerText = '';
-const NG10_1 = document.getElementById('NotaG10_1').innerText = '';
+document.getElementById('NotaG1_1').innerText = grupo1.notas[1];
+document.getElementById('NotaG2_1').innerText = grupo2.notas[1];
+document.getElementById('NotaG3_1').innerText = grupo3.notas[1];
+document.getElementById('NotaG4_1').innerText = grupo4.notas[1];
+document.getElementById('NotaG5_1').innerText = grupo5.notas[1];
+document.getElementById('NotaG6_1').innerText = grupo6.notas[1];
+document.getElementById('NotaG7_1').innerText = grupo7.notas[1];
+document.getElementById('NotaG8_1').innerText = grupo8.notas[1];
+document.getElementById('NotaG9_1').innerText = grupo9.notas[1];
+document.getElementById('NotaG10_1').innerText = grupo10.notas[1];
 
-
+// document.getElementById('NotaG1_2').innerText = grupo1.notas[2];
+// document.getElementById('NotaG2_2').innerText = grupo2.notas[2];
+// document.getElementById('NotaG3_2').innerText = grupo3.notas[2];
+// document.getElementById('NotaG4_2').innerText = grupo4.notas[2];
+// document.getElementById('NotaG5_2').innerText = grupo5.notas[2];
+// document.getElementById('NotaG6_2').innerText = grupo6.notas[2];
+// document.getElementById('NotaG7_2').innerText = grupo7.notas[2];
+// document.getElementById('NotaG8_2').innerText = grupo8.notas[2];
+// document.getElementById('NotaG9_2').innerText = grupo9.notas[2];
+// document.getElementById('NotaG10_2').innerText = grupo10.notas[2];
 
 
 
@@ -62,4 +280,14 @@ const NG10_1 = document.getElementById('NotaG10_1').innerText = '';
 
 
 // Rank Temporário
+document.getElementById("NotaG101_1").innerText = grupo1.mediaProv()
+document.getElementById("NotaG102_1").innerText = grupo2.mediaProv()
+document.getElementById("NotaG103_1").innerText = grupo3.mediaProv()
+document.getElementById("NotaG104_1").innerText = grupo4.mediaProv()
+document.getElementById("NotaG105_1").innerText = grupo5.mediaProv()
+document.getElementById("NotaG106_1").innerText = grupo6.mediaProv()
+document.getElementById("NotaG107_1").innerText = grupo7.mediaProv()
+document.getElementById("NotaG108_1").innerText = grupo8.mediaProv()
+document.getElementById("NotaG109_1").innerText = grupo9.mediaProv()
+document.getElementById("NotaG110_1").innerText = grupo10.mediaProv()
 
