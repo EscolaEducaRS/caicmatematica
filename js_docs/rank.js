156a02f9584946395769bfbf0bbc2426424e68cd
EscolaEducaRS <65333639+EscolaEducaRS@users.noreply.github.com>
2022-10-06 21:55:11 -0300

    Add files via upload

---
+++ js_docs/rank.js
@@ -0,0 +1,63 @@
+let grupo1 = "JYLEGH"
+let grupo2 = "Chico Rafael"
+let grupo3 = "Irmandade"
+let grupo4 = "Os Intocáveis"
+let grupo5 = "Os Elevados"
+let grupo6 = "Mathematics"
+let grupo7 = "Ciclone"
+let grupo8 = "Cocotas"
+let grupo9 = "Sem Cadastro!"
+let grupo10 = "Sem Cadastro!"
+
+const NGrupo1 = document.getElementById('nGrupo1').innerText = grupo1;
+const NGrupo2 = document.getElementById('nGrupo2').innerText = grupo2;
+const NGrupo3 = document.getElementById('nGrupo3').innerText = grupo3;
+const NGrupo4 = document.getElementById('nGrupo4').innerText = grupo4;
+const NGrupo5 = document.getElementById('nGrupo5').innerText = grupo5;
+const NGrupo6 = document.getElementById('nGrupo6').innerText = grupo6;
+const NGrupo7 = document.getElementById('nGrupo7').innerText = grupo7;
+const NGrupo8 = document.getElementById('nGrupo8').innerText = grupo8;
+const NGrupo9 = document.getElementById('nGrupo9').innerText = grupo9;
+const NGrupo10 = document.getElementById('nGrupo10').innerText = grupo10;
+const NGrupo11 = document.getElementById('nGrupo11').innerText = grupo1;
+const NGrupo20 = document.getElementById('nGrupo20').innerText = grupo2;
+const NGrupo30 = document.getElementById('nGrupo30').innerText = grupo3;
+const NGrupo40 = document.getElementById('nGrupo40').innerText = grupo4;
+const NGrupo50 = document.getElementById('nGrupo50').innerText = grupo5;
+const NGrupo60 = document.getElementById('nGrupo60').innerText = grupo6;
+const NGrupo70 = document.getElementById('nGrupo70').innerText = grupo7;
+const NGrupo80 = document.getElementById('nGrupo80').innerText = grupo8;
+const NGrupo90 = document.getElementById('nGrupo90').innerText = grupo9;
+const NGrupo100 = document.getElementById('nGrupo100').innerText = grupo10;
+const NGrupo12 = document.getElementById('nGrupo12').innerText = grupo1;
+const NGrupo21 = document.getElementById('nGrupo21').innerText = grupo2;
+const NGrupo31 = document.getElementById('nGrupo31').innerText = grupo3;
+const NGrupo41 = document.getElementById('nGrupo41').innerText = grupo4;
+const NGrupo51 = document.getElementById('nGrupo51').innerText = grupo5;
+const NGrupo61 = document.getElementById('nGrupo61').innerText = grupo6;
+const NGrupo71 = document.getElementById('nGrupo71').innerText = grupo7;
+const NGrupo81 = document.getElementById('nGrupo81').innerText = grupo8;
+const NGrupo91 = document.getElementById('nGrupo91').innerText = grupo9;
+const NGrupo101 = document.getElementById('nGrupo101').innerText = grupo10;
+
+const NG1_1 = document.getElementById('NotaG1_1').innerText = '0.75';
+const NG2_1 = document.getElementById('NotaG2_1').innerText = '1';
+const NG3_1 = document.getElementById('NotaG3_1').innerText = '1';
+const NG4_1 = document.getElementById('NotaG4_1').innerText = '0.5';
+const NG5_1 = document.getElementById('NotaG5_1').innerText = '0.75';
+const NG6_1 = document.getElementById('NotaG6_1').innerText = '';
+const NG7_1 = document.getElementById('NotaG7_1').innerText = '';
+const NG8_1 = document.getElementById('NotaG8_1').innerText = '';
+const NG9_1 = document.getElementById('NotaG9_1').innerText = '';
+const NG10_1 = document.getElementById('NotaG10_1').innerText = '';
+
+
+
+
+
+
+
+
+
+// Rank Temporário
+
